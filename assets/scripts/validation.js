53fed52e8902841fd2600a2a6c989752a9538f86
JanjanC <janjan_chua@dlsu.edu.ph>
2022-02-10 11:38:04 +0800

    Updated regex for binary input and updated error messages for consistency

--- assets/scripts/validation.js
+++ assets/scripts/validation.js
@@ -4,15 +4,13 @@ function checkBinaryInput() {
     let num2 = $("#bin2").val();
 
     // Check if num1 is a number, not an empty string, or only 0s and 1s for binary
-    if (isNaN(num1) || num1 == "" || !new RegExp(/[0-1]+/).test(num1)) {
+    if (isNaN(num1) || num1 == "" || !new RegExp(/[0-1]+$/).test(num1)) {
         $("#bin1_error").text("Invalid input. Please try again.");
         isValid = false;
     }
     // Check if num1 is within range 0-16 length for binary
     else if ((num1 + "").length > 16 || (num1 + "").length < 2) {
-        $("#bin1_error").text(
-            "Out of range for signed 16 bit integer (must be 2-16 digits)"
-        );
+        $("#bin1_error").text("Out of range for 16-bit signed integer.");
         isValid = false;
     }
     // Clear num1 error messages
@@ -21,15 +19,13 @@ function checkBinaryInput() {
     }
 
     // Check if the num2 is a number, not an empty string, or only 0s and 1s for binary
-    if (isNaN(num2) || num2 == "" || !new RegExp(/[0-1]+/).test(num2)) {
+    if (isNaN(num2) || num2 == "" || !new RegExp(/[0-1]+$/).test(num2)) {
         $("#bin2_error").text("Invalid input. Please try again.");
         isValid = false;
     }
     // Check if num2 is within range -32768 to 32767 for decimal, 0-16 length for binary
     else if ((num2 + "").length > 16 || (num2 + "").length < 2) {
-        $("#bin2_error").text(
-            "Out of range for signed 16 bit integer (must be 2-16 digits)"
-        );
+        $("#bin2_error").text("Out of range for 16-bit signed integer.");
         isValid = false;
     }
     // Clear num2 error messages
@@ -52,7 +48,7 @@ function checkDecimalInput() {
     }
     // Check if num1 is within range -32768 to 32767 for decimal
     else if (num1 < -32768 || num1 > 32767) {
-        $("#dec1_error").text("Out of range for 16 bits");
+        $("#dec1_error").text("Out of range for 16-bit signed integer.");
         isValid = false;
     }
     // Clear num1 error messages
@@ -67,7 +63,7 @@ function checkDecimalInput() {
     }
     // Check if num2 is within range -32768 to 32767 for decimal
     else if (num2 < -32768 || num2 > 32767) {
-        $("#dec2_error").text("Out of range for 16 bits");
+        $("#dec2_error").text("Out of range for 16-bit signed integer.");
         isValid = false;
     }
     // Clear num2 error messages
